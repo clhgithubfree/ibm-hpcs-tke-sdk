dcd3118d7d5390e14c925ba84946d1f4ddc217ed
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2021-09-17 16:00:39 -0400

    extension of the ecdsa.PublicKey struct to support boring crypto results in compilation failing as is the case when using Red Hat go-tools.

--- ep11cmds/verifyOA2Certificate.go
+++ ep11cmds/verifyOA2Certificate.go
@@ -132,7 +132,7 @@ func VerifyOA2Certificate(authToken string, urlStart string, de common.DomainEnt
 	var x, y big.Int
 	x.SetBytes(xbytes)
 	y.SetBytes(ybytes)
-	pubkey := ecdsa.PublicKey{elliptic.P521(), &x, &y}
+	pubkey := ecdsa.PublicKey{Curve: elliptic.P521(), X: &x, Y: &y}
 
 	// Calculate the SHA-512 hash of the certificate body
 	hasher := sha512.New()
